b9c2a565e086eadda47c3aa9cfe82996e0e4fa0d
Ming Man Chan <chanmmn@hotmail.com>
2020-11-16 22:28:14 +0800

    Commit Blazor Hide Div
    Commit Blazor Hide Div

---
+++ BlazorAppDevSpeak/BlazorApp1/Data/ConnectionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class ConnectionModel
+    {
+        public static string strConn = "Server=tcp:youraccount.database.windows.net,1433;Initial Catalog=SSISDB;Persist Security Info=False;User ID=username; " +
+                "Password=password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
